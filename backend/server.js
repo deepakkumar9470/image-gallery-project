ecf45d0e4e208180c28a14c457f419aca67dd8d1
Deepak Kumar Chouhan <dk397787@gmail.com>
2024-02-09 11:12:34 +0530

    add cors fifth commit

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const connDb = require('./db/db')
 //   optionsSuccessStatus: 204,
 // };
 app.use(cors({
-  origin : 'https://image-gallery-project-alpha.vercel.app',
+  origin : ['https://image-gallery-project-alpha.vercel.app','http://localhost:5173/','http://localhost:3000'],
   methods: ['GET,PUT,POST,DELETE'],
 }))
 app.use(express.json())
