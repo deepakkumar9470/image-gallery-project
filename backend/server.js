03dcdc8b41375ff096bdbcfaaf7028ffe0694846
Deepak Kumar Chouhan <dk397787@gmail.com>
2024-02-09 11:24:37 +0530

    add frontend ednpoint

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,8 @@ const PORT = process.env.PORT || 8000
 const connDb = require('./db/db')
 
 //Middlewares
-// const corsOptions = {
-//   origin: 'https://image-gallery-project-alpha.vercel.app',
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   credentials: true,
-//   optionsSuccessStatus: 204,
-// };
 app.use(cors({
-  origin : ['https://image-gallery-project-alpha.vercel.app','http://localhost:5173/','http://localhost:3000'],
+  origin : ['https://image-gallery-project-wine.vercel.app/','http://localhost:5173/','http://localhost:3000'],
   methods: ['GET,PUT,POST,DELETE'],
 }))
 app.use(express.json())
