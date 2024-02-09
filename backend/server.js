3077cfc258b937654ede036eac6c5af1cb6c902f
Deepak Kumar Chouhan <dk397787@gmail.com>
2024-02-09 12:12:37 +0530

    ss

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const connDb = require('./db/db')
 //Middlewares
 app.use(cors({
   origin : ['https://image-gallery-project-wine.vercel.app','http://localhost:5173',
-            'http://localhost:3000','https://soft-capybara-ea7e48.netlify.app/'],
+            'http://localhost:3000','https://soft-capybara-ea7e48.netlify.app'],
   methods: ['GET,PUT,POST,DELETE'],
 }))
 app.use(express.json())
